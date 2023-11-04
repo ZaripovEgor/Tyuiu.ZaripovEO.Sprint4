d0cb2502b15714378c7ece816cc3f21074823e18
Tyuiu.ZaripovEO@outlook.com <zaripov-egor@mail.ru>
2023-11-04 23:09:50 +0500

    Task1

---
+++ Tyuiu.ZaripovEO.Sprint4.Task1.V2.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.ZaripovEO.Sprint4.Task1.V2.Lib;
+
+namespace Tyuiu.ZaripovEO.Sprint4.Task1.V2.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            int[] numsArray = { 1, 7, 2, 2, 6, 2, 3, 2, 3, 5, 7, 7, 1, 2 };
+            int res = ds.Calculate(numsArray);
+            int wait = 34;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
