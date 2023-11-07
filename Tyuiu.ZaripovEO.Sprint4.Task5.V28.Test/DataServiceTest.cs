75ec625f4e9d8ee8148bd1c6abe78ab0490c0b4b
Tyuiu.ZaripovEO@outlook.com <zaripov-egor@mail.ru>
2023-11-07 18:19:54 +0500

    Task5

---
+++ Tyuiu.ZaripovEO.Sprint4.Task5.V28.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.ZaripovEO.Sprint4.Task5.V28.Lib;
+
+namespace Tyuiu.ZaripovEO.Sprint4.Task5.V28.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            int[,] mas = new int[5, 5] { { 3, -6, 4, 6, 6 }, { 7, -5, 5, 5, -8 }, { 8, -3, 6, -7, 7 }, { 5, 3, -8, -4, -5 }, { 3, 7, 4, 3, 8 } };
+            int[,] res = ds.Calculate(mas);
+            int[,] wait = { { 3, 0, 4, 6, 6 }, { 7, 0, 5, 5, 0 }, { 8, 0, 6, 0, 7 }, { 5, 3, 0, 0, 0 }, { 3, 7, 4, 3, 8 } };
+
+            CollectionAssert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.ZaripovEO.Sprint4.Task5.V28.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ZaripovEO.Sprint4.Task5.V28.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ZaripovEO.Sprint4.Task5.V28.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("140398b1-8c2a-4e59-a080-f2ad38e27a54")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
