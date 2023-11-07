2a5a22390dd87407a8ea7799b4ffea9164cb341e
Tyuiu.ZaripovEO@outlook.com <zaripov-egor@mail.ru>
2023-11-07 16:35:19 +0500

    Task4

--- Tyuiu.ZaripovEO.Sprint4.Task4.V21.Test/DataServiceTest.cs
+++ Tyuiu.ZaripovEO.Sprint4.Task4.V21.Test/DataServiceTest.cs
@@ -13,9 +13,9 @@ namespace Tyuiu.ZaripovEO.Sprint4.Task4.V21.Test
         {
             DataService ds = new DataService();
 
-            int[,] mas = new int[3, 3] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+            int[,] mas = new int[5, 5] { { 3, 6, 4, 6, 6 }, { 7, 5, 5, 5, 8 }, { 8, 3, 6, 7, 7 },{ 5, 3, 8, 4, 5 }, { 3, 7, 4, 3, 8 } };
             int res = ds.Calculate(mas);
-            int wait = 20;
+            int wait = 68;
 
             Assert.AreEqual(wait, res);
           
